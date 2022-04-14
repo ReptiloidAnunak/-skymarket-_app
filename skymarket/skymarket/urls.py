3ee69f80568441ecf4921903599e904d6088ce7e
lisvv <81390205+lisvv@users.noreply.github.com>
2022-04-14 09:12:33 +0300

    User creation ready

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -3,10 +3,23 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from djoser.views import UserViewSet
+from rest_framework.routers import SimpleRouter
+
 # TODO здесь необходимо подклюючит нужные нам urls к проекту
 
+users_router = SimpleRouter()
+users_router.register("users", UserViewSet, basename="users")
+
+
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("redoc-tasks/", include("redoc.urls")),
+    path("api/admin/", admin.site.urls),
+    path("api/redoc-tasks/", include("redoc.urls")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path("", include(users_router.urls))
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
